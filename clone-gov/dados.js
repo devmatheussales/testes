e92a69f803fc5b58e729d9eef5b3259b0439a915
Matheus Sales <157637433+devmatheussales@users.noreply.github.com>
2024-02-06 19:23:26 -0300

    article update
    article now show or hide when click or doubleclick at 'numero do cpf'

--- clone-gov/dados.js
+++ clone-gov/dados.js
@@ -1,1 +1,11 @@
-window.alert('Dados enviados com sucesso!')
+
+
+
+var artigo = document.querySelector('article#artigo')
+function clicar()  {
+    artigo.style.display = 'block'
+}
+
+function duplo() {
+    artigo.style.display ='none'
+}
